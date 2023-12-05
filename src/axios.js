ed8bea73371c1a4cad74d2fcf31726165334abdb
SameerGarude <135351542+SameerGarude@users.noreply.github.com>
2023-12-05 18:27:21 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const makeRequest = axios.create({
   baseURL: "http://localhost:8800/api/",
+  baseURL: "https://social-api-jbax.onrender.com/api/",
   withCredentials: true,
 });
