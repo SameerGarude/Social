948392f36c3c246b08ddca657023665a856a934c
SameerGarude <135351542+SameerGarude@users.noreply.github.com>
2023-12-06 18:32:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./style.scss";
-// import NavBar from "./components/navbar/NavBar";
+import NavBar from "./components/navbar/NavBar";
 import LeftBar from "./components/leftBar/LeftBar";
 import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
@@ -28,7 +28,7 @@ function App() {
     return (
       <QueryClientProvider client={queryClient}>
         <div className={`theme-${darkMode ? "dark" : "light"}`}>
-          // <NavBar />
+          <NavBar />
           <div style={{ display: "flex" }}>
             <LeftBar />
             <div style={{ flex: 6 }}>
